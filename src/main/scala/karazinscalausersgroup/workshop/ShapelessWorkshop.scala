5e55f3d2e7ac14c8c25ad69cc7b0d06683eaaf51
Igor Wolkov <igor.wolkov@kredito.de>
2017-09-04 21:42:05 +0300

    03 Coproduct hello world

--- src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
@@ -4,4 +4,23 @@ import shapeless._
 
 object ShapelessWorkshop extends App {
 
+  case class Red()
+  case class Yellow()
+  case class Green()
+
+  type Light = Red :+: Yellow :+: Green :+: CNil
+
+  val red: Light           = Inl(Red())
+  val yellow: Light    = Inr(Inl(Yellow()))
+  val green: Light = Inr(Inr(Inl(Green())))
+
+  def print(light: Light): Unit = light match {
+    case Inl(_)           => println("Red")
+    case Inr(Inl(_))      => println("Yellow")
+    case Inr(Inr(Inl(_))) => println("Green")
+  }
+
+  print(red)
+  print(green)
+
 }
