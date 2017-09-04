82c0a610b8efb234d5b77cdc860c0221ca1f226b
Igor Wolkov <igor.wolkov@kredito.de>
2017-09-04 21:39:32 +0300

    02 HList hello world

--- src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
@@ -4,4 +4,17 @@ import shapeless._
 
 object ShapelessWorkshop extends App {
 
+  val product: String :: Int :: Boolean :: HNil =
+    "hello" :: 42 :: true :: HNil
+
+  val first: String = product.head
+  val second: Int = product.tail.head
+  val rest: Boolean :: HNil = product.tail.tail
+
+  //   Error
+  //   val error = product.tail.tail.tail.head
+
+  val newProduct: Long :: String :: Int :: Boolean :: HNil =
+    42L :: product
+
 }
