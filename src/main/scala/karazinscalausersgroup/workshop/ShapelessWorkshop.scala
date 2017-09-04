b999ce61c1dabae8dd48c5de013196e69ae2de05
Igor Wolkov <igor.wolkov@kredito.de>
2017-09-04 21:48:58 +0300

    05 Coproduct generic example

--- src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
@@ -4,4 +4,16 @@ import shapeless._
 
 object ShapelessWorkshop extends App {
 
+  sealed trait Shape
+  case class Rectangle(width: Double, height: Double) extends Shape
+  case class Circle(radius: Double) extends Shape
+
+  val genShape = Generic[Shape]
+
+  println(genShape.to(Rectangle(3.0, 4.0)))
+  println(genShape.from(Inr(Inl(Rectangle(3.0, 4.0)))))
+
+  println(genShape.to(Rectangle(3.0, 4.0)))
+  println(genShape.from(Coproduct[genShape.Repr](Rectangle(3.0, 4.0))))
+
 }
