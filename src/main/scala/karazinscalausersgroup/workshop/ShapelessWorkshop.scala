92d27e8c8ad851b60fa1a3a07cb27c0e1e138203
Igor Wolkov <igor.wolkov@kredito.de>
2017-09-04 21:53:54 +0300

    03 Coproduct hello world

--- src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
@@ -2,6 +2,8 @@ package karazinscalausersgroup.workshop
 
 import shapeless._
 
+import scala.language.implicitConversions
+
 object ShapelessWorkshop extends App {
 
   case class Red()
@@ -22,5 +24,25 @@ object ShapelessWorkshop extends App {
 
   print(red)
   print(green)
+
+  type StringOrDouble = String :+: Double :+: CNil
+
+  implicit def stringToCoproduct(s: String): StringOrDouble =
+    Coproduct[StringOrDouble](s)
+
+  implicit def doubleToCoproduct(s: Double): StringOrDouble =
+    Coproduct[StringOrDouble](s)
+
+  def print(value: StringOrDouble): Unit =
+    println(
+      value.select[String] getOrElse value.select[Double].get
+    )
+
+
+  print(3.14)
+  print("Hello")
+
+  // Error: will not compile
+  // process(4)
 
 }
