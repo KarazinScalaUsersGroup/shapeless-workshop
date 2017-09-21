a3b8cd2059a5c01257a731643dc25a779fa6d6d5
Igor Wolkov <igor.wolkov@kredito.de>
2017-09-21 13:00:33 +0300

    16 Case class LabeledGeneric example

--- src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
@@ -1,7 +1,18 @@
 package karazinscalausersgroup.workshop
 
 import shapeless._
+import shapeless.labelled._
+import shapeless.ops.hlist._
+import shapeless.syntax.singleton._
 
 object ShapelessWorkshop extends App {
 
+  case class Document(path: String, size: Int, readOnly: Boolean)
+
+  val document = Document("/", 42, readOnly = true)
+
+  val docGen = LabelledGeneric[Document]
+
+  println(docGen.to(document))
+  println(docGen.from('path ->> "/" :: 'size ->> 42 :: 'readOnly ->> true :: HNil))
 }
