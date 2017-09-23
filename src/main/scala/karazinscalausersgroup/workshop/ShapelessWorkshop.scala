702eed63404e6d75e3637406078e3f970faf5b58
Igor Wolkov <igor.wolkov@kredito.de>
2017-09-23 07:48:57 +0300

    Add cats dependency

--- src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/ShapelessWorkshop.scala
@@ -1,6 +1,12 @@
 package karazinscalausersgroup.workshop
 
 import shapeless._
+import shapeless.ops.hlist
+import cats.Monoid
+import cats.instances.all._
+import shapeless.record._
+
+import shapeless.labelled.{field, FieldType}
 
 object ShapelessWorkshop extends App {
 
